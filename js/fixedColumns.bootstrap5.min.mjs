98c64227aecabc080442bf16f2b17891eb35889e
Allan Jardine <allan@datatables.net>
2022-10-10 19:49:07 +0000

    Add support for mjs files (ES Modules)

---
+++ js/fixedColumns.bootstrap5.min.mjs
@@ -0,0 +1,1 @@
+import $ from"jquery";import DataTable from"datatables.net-bs5";import DataTable from"datatables.net-fixedcolumns";export default DataTable;
---
+++ js/fixedColumns.bootstrap5.mjs
@@ -0,0 +1,13 @@
+
+/*! Bootstrap 5 integration for DataTables' FixedColumns
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-bs5';
+import DataTable from 'datatables.net-fixedcolumns';
+
+
+
+
+export default DataTable;

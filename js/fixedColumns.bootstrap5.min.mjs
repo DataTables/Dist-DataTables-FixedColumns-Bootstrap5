c081103c29abc01faa87bc02d603636640442966
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:13:31 +0000

    7bc6dcd5053b2d6128798325bbdf202dc14ef38c Release 4.2.1 (packaging error with 4.2.0)
    Sync to source repo @7bc6dcd5053b2d6128798325bbdf202dc14ef38c

--- js/fixedColumns.bootstrap5.min.mjs
+++ js/fixedColumns.bootstrap5.min.mjs
@@ -1,4 +1,4 @@
 /*! Bootstrap 5 integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bs5";import DataTable from"datatables.net-fixedcolumns";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-bs5";import"datatables.net-fixedcolumns";export default DataTable;
--- js/fixedColumns.bootstrap5.mjs
+++ js/fixedColumns.bootstrap5.mjs
@@ -1,11 +1,10 @@
-
 /*! Bootstrap 5 integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bs5';
-import DataTable from 'datatables.net-fixedcolumns';
+import 'datatables.net-fixedcolumns';
 
 
 

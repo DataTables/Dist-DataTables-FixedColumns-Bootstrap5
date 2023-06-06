f5fab3cb9686b8527d254386317437731160236a
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-06-06 11:06:22 +0100

    cad68434135f57d4d30f74d27b9e9dfaed9c4894 Fix: Memory leak as event listeners we not being removed
    https://datatables.net/forums/discussion/76449
    
    Sync to source repo @cad68434135f57d4d30f74d27b9e9dfaed9c4894

--- js/fixedColumns.bootstrap5.min.mjs
+++ js/fixedColumns.bootstrap5.min.mjs
@@ -1,4 +1,4 @@
 /*! Bootstrap 5 integration for DataTables' FixedColumns
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bs5";import FixedColumns from"datatables.net-fixedcolumns";export default DataTable;
+import jQuery from"jquery";import DataTable from"datatables.net-bs5";import FixedColumns from"datatables.net-fixedcolumns";let $=jQuery;export default DataTable;
--- js/fixedColumns.bootstrap5.mjs
+++ js/fixedColumns.bootstrap5.mjs
@@ -2,9 +2,12 @@
  * © SpryMedia Ltd - datatables.net/license
  */
 
-import $ from 'jquery';
+import jQuery from 'jquery';
 import DataTable from 'datatables.net-bs5';
 import FixedColumns from 'datatables.net-fixedcolumns';
+
+// Allow reassignment of the $ variable
+let $ = jQuery;
 
 
 
